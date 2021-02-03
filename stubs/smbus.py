cc630381507ab5052052aacb7008f5c006b94e25
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-02-03 08:41:47 +0000

    added stub for smbus module

---
+++ stubs/smbus.py
@@ -0,0 +1,41 @@
+# stubs for smbus module
+from random import randint
+
+class _SMBus(object):
+
+    def __init__(self, bus=None, force=False):
+        self.bus = bus
+        self.force = force
+
+    def write_byte_data(self, i2c_addr, register, value):
+        pass
+
+    def write_byte(self, i2c_addr, value):
+        pass
+
+    def read_byte_data(self, i2c_addr, register):
+        return randint(0, 2**8-1)
+
+    def read_byte(self, i2c_addr):
+        return randint(0, 2**8-1)
+
+    def read_word_data(self, i2c_addr, register):
+        return randint(0, 2**16-1)
+
+    def write_word_data(self, i2c_addr, register, value):
+        pass
+
+    def read_i2c_block_data(self, i2c_addr, register, len):
+        return [randint(0, 2**8-1)]*len
+
+    def write_i2c_block_data(self, i2c_addr, register, data):
+        pass
+
+    def open(self, bus):
+        pass
+
+    def close(self):
+        pass
+
+
+SMBus = _SMBus()

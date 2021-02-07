5735263b74cf1ebb4242a934dcdfe3bb6937f7b0
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-02-07 18:16:34 +0000

    added docstrings to functions

--- stubs/RPI/GPIO.py
+++ stubs/RPI/GPIO.py
@@ -1,3 +1,26 @@
+# Stub of RPi.GPIO. Original from https://sourceforge.net/projects/raspberry-gpio-python/
+# 
+# Copyright (c) 2012-2021 Ben Croston
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""stub: This package provides a Python module to control the GPIO on a Raspberry Pi"""
 
 from random import randint
 
@@ -46,6 +69,7 @@ RPI_INFO = {
 
 # Define PWM
 class PWM(object):
+    '''Pulse Width Modulation class'''
 
     def __init__(self, channel, frequency):
         self.channel = channel
@@ -53,53 +77,127 @@ class PWM(object):
         self.dc = 0
 
     def start(self, dutycycle):
+        '''Start software PWM dutycycle - the duty cycle (0.0 to 100.0)'''
         pass
 
     def ChangeDutyCycle(self, dutycycle):
+        '''Change the duty cycle dutycycle - between 0.0 and 100.0'''
         self.dc = dutycycle
 
     def ChangeFrequency(self, frequency):
+        '''Change the frequency frequency - frequency in Hz (freq > 1.0)'''
         self.frequency = frequency
 
     def stop(self):
+        '''Stop software PWM'''
         pass
 
 # Define functions
 def cleanup(channel=None):
+    '''
+    Clean up by resetting all GPIO channels that have been used by this program
+    to INPUT with no pullup/pulldown and no event detection
+    [channel] - individual channel or list/tuple of channels to clean up.
+    Default - clean every channel that has been used.
+    '''
     pass
 
 def setup(channel, direction, initial=0, pull_up_down=PUD_OFF):
+    '''
+    Set up a GPIO channel or list of channels with a direction and (optional)
+    pull/up down control
+    channel - either board pin number or BCM number depending on which mode is set.
+    direction - IN or OUT
+    [pull_up_down] - PUD_OFF (default), PUD_UP or PUD_DOWN
+    [initial] - Initial value for an output channel
+    '''
     pass
 
 def output(channel, value):
+    '''
+    Output to a GPIO channel or list of channels
+    channel - either board pin number or BCM number depending on which mode is set.
+    value - 0/1 or False/True or LOW/HIGH
+    '''
     pass
 
 def input(channel):
+    '''
+    Input from a GPIO channel. Returns HIGH=1=True or LOW=0=False
+    channel - either board pin number or BCM number depending on which mode is set.
+    '''
     return randint(0, 1)
 
 def setmode(mode):
+    '''
+    Set up numbering mode to use for channels.
+    BOARD - Use Raspberry Pi board numbers
+    BCM - Use Broadcom GPIO 00..nn numbers
+    '''
     pass
 
 def getmode():
+    '''
+    Get numbering mode used for channel numbers. Returns BOARD, BCM or None
+    '''
     return BCM
 
-def add_event_callback(gpio, callback):
+def add_event_callback(channel, callback):
+    '''
+    Add a callback for an event already defined using add_event_detect()
+    channel - either board pin number or BCM number depending on which mode is set.
+    callback - a callback function
+    '''
     pass
 
-def add_event_detect(gpio, edge, callback=None, bouncetime=None):
+def add_event_detect(channel, edge, callback=None, bouncetime=None):
+    '''
+    param optional
+
+    Enable edge detection events for a particular GPIO channel.
+    channel      - either board pin number or BCM number depending on which mode is set.
+    edge         - RISING, FALLING or BOTH
+    [callback]   - A callback function for the event (optional)
+    [bouncetime] - Switch bounce timeout in ms for callback
+    '''
     pass
 
-def remove_event_detect(gpio):
+def remove_event_detect(channel):
+    '''
+    Remove edge detection for a particular GPIO channel
+    channel - either board pin number or BCM number depending on which mode is set.
+    '''
     pass
 
 def event_detected(channel):
+    '''
+    Returns True if an edge has occurred on a given GPIO.  You need to enable
+    edge detection using add_event_detect() first.
+    channel - either board pin number or BCM number depending on which mode is set.
+    '''
     return False
 
 def wait_for_edge(channel, edge, bouncetime=None, timeout=None):
+    '''
+    Wait for an edge.  Returns the channel number or None on timeout.
+    channel      - either board pin number or BCM number depending on which mode is set.
+    edge         - RISING, FALLING or BOTH
+    [bouncetime] - time allowed between calls to allow for switchbounce
+    [timeout]    - timeout in ms
+    '''
     pass
 
 def gpio_function(channel):
+    '''
+    param IN
+
+    Return the current GPIO function (IN, OUT, PWM, SERIAL, I2C, SPI)
+    channel - either board pin number or BCM number depending on which mode is set.
+    '''
     return OUT
 
 def setwarnings(state):
+    '''
+    Enable or disable warning messages
+    '''
     pass
